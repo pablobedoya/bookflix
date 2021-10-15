2b43a4d87f061cd3215aa1655e361f00dc1182fb
Pablo Bedoya <pablo.reds@hotmail.com>
2021-10-15 02:22:35 -0300

    Adding login resource

---
+++ src/main/java/com/br/bookflix/login/UserLogin.java
@@ -0,0 +1,12 @@
+package com.br.bookflix.login;
+
+import lombok.Getter;
+
+@Getter
+public class UserLogin {
+
+	private String email;
+	
+	private String password;
+	
+}
---
+++ src/main/java/com/br/bookflix/login/service/LoginServiceImpl.java
@@ -0,0 +1,35 @@
+package com.br.bookflix.login.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+
+import com.br.bookflix.exception.BookflixException;
+import com.br.bookflix.exception.InternalServerError;
+import com.br.bookflix.exception.UnauthorizedException;
+import com.br.bookflix.login.UserLogin;
+import com.br.bookflix.user.User;
+import com.br.bookflix.user.repository.UserRepository;
+
+@Service
+public class LoginServiceImpl implements LoginService {
+
+	@Autowired
+	private UserRepository repository;
+
+	@Override
+	public User login(UserLogin userLogin) throws BookflixException {
+		try {
+			User user = repository.getByEmail(userLogin.getEmail());
+			if (user != null && BCrypt.checkpw(userLogin.getPassword(), user.getPassword())) {
+				return user;
+			}
+			throw new UnauthorizedException("Wrong credentials", "Incorrect email or password");
+		} catch (UnauthorizedException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new InternalServerError("Unexpected error", e.getMessage());
+		}
+	}
+
+}
